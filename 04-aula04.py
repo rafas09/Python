20bd697f7f8e690cd0e33d6c2134ab15327d72d0
Rafaella Ferreira Monteiro <rafaellamonteiro555@gmail.com>
2024-08-29 11:52:00 -0300

    Add files via upload

---
+++ 04-aula04.py
@@ -0,0 +1,15 @@
+# Atribuição de variáveis
+nome = "Rafinha"
+idade = 15
+altura = 1.59
+
+#exibir mensagem na tela 
+print("Nome:",nome,"idade:",idade,"altura",altura)
+# Formatação com f-string
+print(f"nome: {nome} idade:{idade} altura: {altura}")
+
+nome = input("Digite o seu nome:")
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura:"))
+
+print(f"nome: {nome} idade:{idade} altura: {altura}")
