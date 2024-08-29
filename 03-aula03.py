3dee16d74c8f8fb1ad9741988b153717d25bbef5
Rafaella Ferreira Monteiro <rafaellamonteiro555@gmail.com>
2024-08-29 11:51:40 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,16 @@
+# Variáveis em Python
+# Não há necessidade de declará-las no inicio do progama
+# Basta criar no meio do algoritmo com a atribuição
+
+teste = "Rafaella"
+print(teste) #Imprime a string "Rafaella"
+print(type (teste)) #Imprime o tipo da variável, string
+teste = 15
+print(teste) #Imprime o número 10
+print(type (teste)) #Imprime o tipo da variável, que é int
+teste = 1.74
+print(teste) #Imprime o número 1.74
+print(type (teste)) #Imprime o tipo da variável, que é float
+teste = True
+print(teste) #Imprime o valor true
+print(type (teste)) #Imprime o tipo da variável, que é bool
