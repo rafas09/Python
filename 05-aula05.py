a0df7dcfbdaa7d05a8ad5c2dc2186c030dcd9ee4
Rafaella Ferreira Monteiro <rafaellamonteiro555@gmail.com>
2024-09-10 11:16:21 -0300

    Add files via upload

---
+++ 05-aula05.py
@@ -0,0 +1,13 @@
+# Tipos de dados em Python
+# Inteiros
+inteiro = 10
+print(inteiro)
+# Floats
+float = 10.5
+print(float)
+# Stings
+string = "Olá, Mundo!"
+print(string)
+# Booleanos
+booleano = True
+print(booleano)
